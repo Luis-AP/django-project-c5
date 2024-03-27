b33be0f79a86f74d02d1b4c109825619c239bf33
Luis-AP <lap18958@gmail.com>
2024-03-26 21:40:15 -0300

    URL y viste de ejemplo

--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+import datetime
+
 
 # Create your views here.
+def home(request):
+    now = datetime.datetime.now()
+    return HttpResponse(
+        f"Bienvenido a primera página web. La hora del servidor es {now.strftime('%A, %d %B, %Y a las %X')}."
+    )
