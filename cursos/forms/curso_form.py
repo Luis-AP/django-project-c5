6bfa22cd0254b87c3a2645a8c95e7ee642ea4908
Luis-AP <lap18958@gmail.com>
2024-04-09 22:40:16 -0300

    Formulario para ABM de cursos

---
+++ cursos/forms/curso_form.py
@@ -0,0 +1,68 @@
+from django import forms
+from ..models import Curso, Instructor, Categoria
+
+
+class CursoForm(forms.ModelForm):
+    instructor = forms.ModelChoiceField(
+        queryset=Instructor.objects.all(),
+        required=False,
+        empty_label="Seleccione el profesor",
+        widget=forms.Select(attrs={"class": "form-control"}),
+    )
+    categoria = forms.ModelChoiceField(
+        queryset=Categoria.objects.all(),
+        required=False,
+        empty_label="Seleccione la categoría",
+        widget=forms.Select(attrs={"class": "form-control"}),
+    )
+
+    class Meta:
+        model = Curso
+        fields = [
+            "nombre",
+            "descripcion",
+            "precio",
+            "fecha_publicacion",
+            "instructor",
+            "categoria",
+            "duracion",
+            "estado",
+            "destacado",
+        ]
+        labels = {
+            "nombre": "Nombre del curso",
+            "descripcion": "Descripción",
+            "precio": "Precio",
+            "fecha_publicacion": "Fecha de publicación",
+            "instructor": "Instructor",
+            "categoria": "Categoría",
+            "duracion": "Duración",
+            "estado": "Estado",
+            "destacado": "Destacado",
+        }
+        widgets = {
+            "nombre": forms.TextInput(
+                attrs={
+                    "placeholder": "Ingrese nombre del curso",
+                    "class": "form-control",
+                }
+            ),
+            "descripcion": forms.Textarea(
+                attrs={"placeholder": "Ingrese la descripción", "class": "form-control"}
+            ),
+            "precio": forms.NumberInput(
+                attrs={"placeholder": "Ingrese el precio", "class": "form-control"}
+            ),
+            "fecha_publicacion": forms.NumberInput(
+                attrs={
+                    "type": "date",
+                    "placeholder": "Ingrese la fecha",
+                    "class": "form-control",
+                }
+            ),
+            "duracion": forms.TimeInput(
+                attrs={"placeholder": "Ingrese la duración", "class": "form-control"}
+            ),
+            "estado": forms.Select(attrs={"class": "form-control"}),
+            "destacado": forms.CheckboxInput(attrs={"class": "form-check-input"}),
+        }
--- cursos/urls.py
+++ cursos/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path("", home, name="home"),
     path("cursos/", curso_list, name="curso_list"),
     path("cursos/<int:curso_id>/", curso_detail, name="curso_detail"),
+    path("cursos/crear/", create_curso, name="create_curso"),
+    path("cursos/<int:curso_id>/actualizar/", update_curso, name="curso_update"),
+    path("cursos/<int:curso_id>/eliminar/", delete_curso, name="curso_delete"),
+    path("cursos/<int:curso_id>/ocultar/", hide_curso, name="hide_curso"),
 ]
